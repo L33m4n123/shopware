5450c447a52cba671c96392ed41cd3417071226d
Patrick Lehmann <lehmann.s.patrick@gmail.com>
2018-10-07 04:31:45 +0200

    First controller

---
+++ custom/plugins/Startup/Controllers/Frontend/Startup.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Leeeeman
+ * Date: 07.10.2018
+ * Time: 04:28
+ */
+
+class Shopware_Controllers_Frontend_Startup extends Enlight_Controller_Action
+{
+    public function indexAction()
+    {
+        var_dump("Es klappt!");
+        exit;
+    }
+}
