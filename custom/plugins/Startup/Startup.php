d6254f47ad91b9722977d93f7fb2c43be20223d5
Patrick Lehmann <lehmann.s.patrick@gmail.com>
2018-10-07 04:14:34 +0200

    First plugin

---
+++ custom/plugins/Startup/Startup.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Leeeeman
+ * Date: 07.10.2018
+ * Time: 04:12
+ */
+
+namespace Startup;
+
+
+use Shopware\Components\Plugin;
+
+class Startup extends Plugin
+{
+
+}
