133cad448d551f27f3ddae349675e5ba9be28d66
Patrick Lehmann <lehmann@vonaffenfels.de>
2018-10-07 03:57:03 +0200

    Initial setup

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php return array (
+  'db' => 
+  array (
+    'host' => 'localhost',
+    'port' => '3306',
+    'username' => 'shopwareuser',
+    'password' => 'Aabzzti0',
+    'dbname' => 'shopware',
+  ),
+);
